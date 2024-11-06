a9e7a723751b49c7b885b524b6953cb027a64908
TomE137 <tomeliyahu@gmail.com>
2024-11-06 19:14:04 +0200

    added ,  in billl3 output. numwords code will now also count thousands when counting hundreds

--- Bill3.java
+++ Bill3.java
@@ -3,7 +3,7 @@ public class Bill3 {
 	public static void main(String[] args) {
 	    String name1 = args[0], name2 = args[1], name3 = args[2];
 		int price = Integer.valueOf(args[3]);
-		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 + 
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + 
 							": pay " + Math.ceil((double) price/3) + " Shekels each");   
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -4,7 +4,8 @@ public class NumWords {
 		int number = Integer.parseInt(args[0]);
 		int ones = number % 10;
 		int tens = (number % 100 - ones)/10;
-		int hundreds = (number % 1000 - tens - ones)/100;
+		int hundreds = (number - tens - ones)/100;
+		// int hundreds = (number % 1000 - tens - ones)/100;
 		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
 }

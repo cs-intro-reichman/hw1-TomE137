3a5f5c087eff03146f92eb19f57f7ca0315476a7
TomE137 <tomeliyahu@gmail.com>
2024-11-06 21:34:14 +0200

    updated timeformat
    updated formula to improve efficiency

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,8 +1,6 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
 	public static void main(String[] args) {
-		// Ive tried not to use an if statement as weve yet to cover it's use cases in class
-
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
 		String addedZero = "";
@@ -10,11 +8,13 @@ public class TimeFormat {
 		if (minutes<10) {
 			addedZero = "0";			
 		}
+
 		if (hours < 12) {
 			System.out.println(hours + ":" + addedZero + minutes + " AM");
-		}
+		}		
 		else {
-			System.out.println(hours-12 + ":" + addedZero + minutes + " PM");
+			// (int) (hours-1)/12 will output 0 if hours is 12 and 1 if hours > 12 thus preventing 0:xx pm for 12 without adding another if
+			System.out.println(hours-12*((int) (hours-1)/12) + ":" + addedZero + minutes + " PM");
 		}
 	}
 }
